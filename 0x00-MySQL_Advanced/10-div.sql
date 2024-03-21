f912a238fc7992afdc976d9e010eb8bb97b2371e
Yeeloman <yami.onlyme@gmail.com>
2024-03-21 23:07:51 +0000

    8&9

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -13,8 +13,8 @@ BEGIN
 	DECLARE result FLOAT;
 	IF b = 0 THEN
 		RETURN 0;
-        END IF;
-        SET result = (a * 1.0) / b;
-        RETURN result;
+    END IF;
+    SET result = (a * 1.0) / b;
+    RETURN result;
 END;$$
 DELIMITER ;
