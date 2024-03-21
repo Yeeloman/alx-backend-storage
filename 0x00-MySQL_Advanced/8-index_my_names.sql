20d104b4619cac9cc1054a06032b44b18663b492
Yeeloman <yami.onlyme@gmail.com>
2024-03-21 22:50:00 +0000

    9

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,3 +1,3 @@
 -- a SQL script that creates an index idx_name
 --_first on the table names and the first letter of name.
-CREATE INDEX idx_name ON names(name(1));
+CREATE INDEX idx_name ON names (name(1));
