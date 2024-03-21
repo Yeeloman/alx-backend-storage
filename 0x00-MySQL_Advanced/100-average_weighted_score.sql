ecff315cffa650977b32c892556120a271e2364e
Yeeloman <yami.onlyme@gmail.com>
2024-03-21 23:37:38 +0000

    12

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -6,8 +6,9 @@ CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
     UPDATE users
     SET average_score = (
-        SELECT SUM(score * weight) / SUM(weight)
+        SELECT IF(SUM(score * weight) > 0, SUM(score * weight) / SUM(weight), 0)
         FROM corrections
+        JOIN projects ON corrections.project_id = projects.id
         WHERE corrections.user_id = user_id
     )
     WHERE id = user_id;
