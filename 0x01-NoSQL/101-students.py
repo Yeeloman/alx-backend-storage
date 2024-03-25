4e9cdce9cd85bee4f529a7ac6464727bd81232d2
Yeeloman <yami.onlyme@gmail.com>
2024-03-25 15:24:39 +0000

    add task 101

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -6,14 +6,18 @@ def top_students(mongo_collection):
     """ function that returns all
     students sorted by average score"""
     pipline = [
-        {"$unwind": "$topics"},
         {
-            "$group": {
-                "_id": "$_id",
-                "name": {"$first": "$name"},
-                "averageScore": {"$avg": "$topics.score"}
-            }
+            "$project":
+                {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
         },
-        {"$sort": {"averageScore": -1}}
+        {
+            "$sort":
+                {
+                    "averageScore": -1
+                }
+        }
     ]
     return mongo_collection.aggregate(pipline).sort("averageScore", -1)
