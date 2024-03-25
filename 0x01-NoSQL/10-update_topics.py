4f63088dfdaf153f4054e78e9043be40233a5b12
Yeeloman <yami.onlyme@gmail.com>
2024-03-25 15:15:49 +0000

    forgot return in 10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -5,5 +5,4 @@
 def update_topics(mongo_collection, name, topics):
     """function that changes all topics of a school
     document based on the name"""
-    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
-    pass
+    return mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
